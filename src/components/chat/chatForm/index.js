f25f42a3a98c18e9e73bea247b35caa55370ab3e
simonprod <xeywar@gmail.com>
2019-06-04 14:03:17 +0300

    Add ChatForm component

---
+++ src/components/chat/chatForm/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class ChatForm extends Component {
+
+    render() {
+
+    	return (
+            <div className="combat__chat__form">
+                <form>
+                    <input />
+                </form>
+            </div>
+        )
+
+    }
+
+}
+
+export default ChatForm;
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import LobyComponent from '../../components/combat/loby/index';
 import ListMessageComponent from '../../components/chat/listMessage/index';
 import ListWhoOnline from '../../components/chat/whoOnline/index';
+import ChatForm from '../../components/chat/chatForm/index';
 import '../../components/combat/index.css';
 
 class Home extends Component {
@@ -27,7 +28,10 @@ class Home extends Component {
             <div className="combat">
                 <LobyComponent></LobyComponent>
                 <div className="combat__chat">
-                    <ListMessageComponent></ListMessageComponent>
+                    <div className="combat__chat__messages">
+                        <ListMessageComponent></ListMessageComponent>
+                        <ChatForm></ChatForm>
+                    </div>
                     <ListWhoOnline></ListWhoOnline>
                 </div>
             </div>
