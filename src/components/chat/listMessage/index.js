efd4b95735262c4bc8a66af972853febd1fc122d
Oleg <ben.oleg.i.1.17@gmail.com>
2019-06-04 19:46:27 +0300

    OlegBen-(Add ListMessage component)

--- src/components/chat/listMessage/index.js
+++ src/components/chat/listMessage/index.js
@@ -1,17 +1,83 @@
 import React, { Component } from 'react';
 import './index.css';
+import authHelpers from '../../auth/authHelpers'
+import config from '../../../config'
 
 class ListMessage extends Component {
 
-    render() {
+    constructor(props) {
+        super(props);
 
-    	return (
-            <div className="combat__chat__who-online">
+        this.state = {
+            messages: []
+        }
+    }
+
+    //Функция жизненого цикла, реагирующая на обновление компонента, при возврате true, обновляет компонент
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.state.messages.length !== nextState.messages.length;
+    }
+
+    render() {
+        setInterval(() => {
+            this.getMessages();
+        }, 1000); //Запрос к серверу, повторяющийся через определённый промежуток времени
+
+        return (
+            //Список сообщений
+            <div className="combat__chat__message-list">
                 Список сообщений
-                { /* тут нужно пилить код */ }
+                <ul className="message-list">
+                    {this.state.messages.map((message) => {
+                        let date = (new Date(message.timestamp));
+                        let time = date.getHours() + ':' + date.getMinutes();
+
+                        return (
+                            <li key={message.timestamp} className="message-list__message">
+                                {time} [<span style={{color:this.getRandColor()}}>{message.user.username}</span>]: {message.message}
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         )
 
+    }
+
+    //Функция для получения массива сообщений, используя Api ручку
+    getMessages = () => {
+        let userToken = authHelpers.getToken(); //Получение пользовательского токена
+        if(userToken) {
+            fetch(config.backend + '/api/chat?token=' + userToken)
+                .then(res => res.json())
+                .then(data => {
+                    if(data.status === 'error') return;
+                    this.setState({
+                        //messages: data.chat
+
+                        //Тест больших сообщений
+                        messages: [{"message":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.","timestamp":1559644898210,"user":{"id":"JtG_KW","username":"xeywar","last_active":1579657926276}}, {"message":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.","timestamp":1559661300,"user":{"id":"JtG_KW","username":"xeywar","last_active":1559655926278}}]
+                    })
+                })
+        }
+    };
+
+    //Функция для порокрутки к последнему сообщению
+    scroll = () => {
+        let messageList = document.querySelector('.message-list');
+        if(messageList) {
+            messageList.scrollTop = messageList.scrollHeight;
+        }
+    };
+
+    //Функция окраски ника в случайный цвет
+    getRandColor = () => {
+        return 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
+    };
+
+    //Функция жизненого цикла, которая выполнится после обновления компонента
+    componentDidUpdate() {
+        this.scroll()
     }
 
 }
