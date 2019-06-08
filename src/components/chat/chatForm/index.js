05a79e32824c4023f4e9f74bfa23ccfafb5e6f10
vlada <vladka.96@mail.ru>
2019-06-08 13:29:02 +0300

    glazok.add chatForm comtonent

--- src/components/chat/chatForm/index.js
+++ src/components/chat/chatForm/index.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
 import './index.css';
+import authHelpers from '../../auth/authHelpers'
+import config from '../../../config'
 
 class ChatForm extends Component {
 
+	 constructor(props) {
+        super(props);
+
+        this.state = {
+            message: ''
+        }
+    }
+
+	sendMessage = () => {
+		console.log(this.state.message);
+
+		 let userToken = authHelpers.getToken(); //Получение пользовательского токена
+        if(userToken) {
+            fetch(config.backend + '/api/chat',{
+            	method: 'POST',
+            	  headers: {
+				      'Accept': 'application/json',
+				      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+				    },
+            	body: JSON.stringify({
+            		message: this.state.message,
+            		token: userToken
+            	})
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if(data.status === 'error') {
+                        console.error(data);
+                        return;
+                    }
+
+                    this.setState({
+                        message: ''
+					 })
+                })
+        }
+	}
+	 handleChange = (event) => {
+	    this.setState({message: event.target.value});
+	  }
     render() {
 
     	return (
             <div className="combat__chat__form">
-                <form>
-                    <input />
-                </form>
+                    <input className="chat__input" value={this.state.message} onChange={this.handleChange} />
+                    <button onClick={this.sendMessage}>
+                    Отправить
+                    </button>
+          
             </div>
         )
 
